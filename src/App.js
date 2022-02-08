abc146fbf180e97f3810d056a1eca1a545b503cf
Soobin <ssbin93@gmail.com>
2022-02-08 17:21:26 +1000

    Start practicing with the styled component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { keyframes } from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container>
+      <Box color="teal" />
+      <Circle color="tomato" />
+      <MovingBox color="yellow" />
+    </Container>
+  )
 }
+
+const rotation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0;
+  }
+  
+  50% {
+    border-radius: 100px;
+  }
+  
+  100% {
+    transform: rotate(360deg);
+    border-radius: 0;
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Box = styled.div`
+  background-color: ${(props) => props['color']};
+  width: 100px;
+  height: 100px;
+  margin: 14px;
+  
+  &:hover {
+    width: 150px;
+    height: 150px;
+  }
+  
+  &:active {
+    opacity: 50%;
+  }
+`;
+
+const Circle = styled(Box)`
+  border-radius: 50px;
+`;
+
+const MovingBox = styled(Box)`
+  animation: ${rotation} 1s linear infinite;
+`;
 
 export default App;
