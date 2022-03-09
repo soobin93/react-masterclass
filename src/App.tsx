c16eae6cdd18c5388ca6b9bdb75f8b423b88c512
Soobin Son <soobs@Soobins-MacBook-Pro.local>
2022-03-09 16:54:10 +1000

    Add typescript

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Circle from "./Circle";
+
+function App() {
+  return (
+    <div>
+      <Circle bgColor="red" />
+      <Circle bgColor="blue" />
+      <Circle bgColor="green" />
+    </div>
+  );
+}
+
+export default App;
