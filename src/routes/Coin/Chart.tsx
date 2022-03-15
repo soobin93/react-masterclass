1e710bd49b35904f40c73a010f5d3f6dc2df954c
soobin93 <ssbin93@gmail.com>
2022-03-15 22:23:05 +1000

    Add nested routes for coin page

---
+++ src/routes/Coin/Chart.tsx
@@ -0,0 +1,5 @@
+const Chart = () => {
+  return <h1>This is chart!</h1>;
+};
+
+export default Chart;
---
+++ src/routes/Coin/Price.tsx
@@ -0,0 +1,5 @@
+const Price = () => {
+  return <h1>This is price!</h1>;
+};
+
+export default Price;
